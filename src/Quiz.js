16d8dd074081f4b0c62c0140acd4b6d5afe68041
MrFusion3000 <nico@crepro.com>
2021-02-11 11:53:48 +0100

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,7 +11,7 @@ class Quiz extends Component {
     this.state = { quiz_position: 1 }
   }
   render() {
-    const [isQuizEnd] = useState(0)
+//    const [isQuizEnd] = useState(0)
     return (
       <div>
         <QuizEnd/>
