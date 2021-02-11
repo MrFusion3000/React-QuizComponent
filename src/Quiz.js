43e930a341bfb6557db9e3ad3574a150a5b7ee3f
MrFusion3000 <nico@crepro.com>
2021-02-11 13:44:30 +0100

    Update 6.2

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
 import QuizQuestion from './QuizQuestion.js'
 import QuizEnd from './QuizEnd.js'
-import {stat} from 'fs';
+//import { stat } from 'fs';
 
 let quizData = require('./quiz_data.json')
-
 
 class Quiz extends Component {
   constructor(props) {
